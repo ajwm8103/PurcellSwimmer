5525cd44cdb25039a27dbe9dc8c28d95c9b90945
Andrew Magnuson <ajwm8103@gmail.com>
2022-11-27 15:20:45 -0500

    Updated FluidDrag

--- Assets/Scripts/FluidDrag.cs
+++ Assets/Scripts/FluidDrag.cs
@@ -11,6 +11,7 @@ public class FluidDrag : MonoBehaviour
     private float sa_x;
     private float sa_y;
     private float sa_z;
+    private float C_D = 1.05f;
 
     // Use this for initialization
     void Start()
@@ -23,12 +24,22 @@ public class FluidDrag : MonoBehaviour
         sa_z = transform.localScale.x * transform.localScale.y;
     }
 
+    float LinearDragFloat(float A, float dot){
+        return A * dot * viscosityDrag * C_D;
+    }
+    float QuadraticDragFloat(float A, float dot)
+    {
+        return A * dot * Mathf.Pow(viscosityDrag,2) * C_D;
+    }
+
     void FixedUpdate()
     {
+        // F_drag = 0.5 * C_D * A * rho * v^2
         // Cache positive axis vectors:
         Vector3 forward = transform.forward;
         Vector3 up = transform.up;
         Vector3 right = transform.right;
+
         // Find centers of each of box's faces
         Vector3 xpos_face_center = (right * transform.localScale.x / 2) + transform.position;
         Vector3 ypos_face_center = (up * transform.localScale.y / 2) + transform.position;
@@ -42,21 +53,19 @@ public class FluidDrag : MonoBehaviour
         //=== Apply Opposing Force ----------------------------------------
 
         // FRONT (posZ):
-        Vector3 pointVelPosZ = rigidBod.GetPointVelocity(zpos_face_center); // Get velocity of face's center (doesn't catch torque around center of mass)
-        Vector3 fluidDragVecPosZ = -forward *     // in the direction opposite the face's normal
-                                    Vector3.Dot(forward, pointVelPosZ)   // get the proportion of the velocity vector in the direction of face's normal
-                                    * sa_z * viscosityDrag;   // multiplied by face's surface area, and user-defined multiplier
+        Vector3 pointVelPosZ = rigidBod.GetPointVelocity(zpos_face_center);
+        Vector3 fluidDragVecPosZ = -forward * LinearDragFloat(sa_z, Vector3.Dot(forward, pointVelPosZ));
         rigidBod.AddForceAtPosition(fluidDragVecPosZ * 2, zpos_face_center);  // Apply force at face's center, in the direction opposite the face normal
                                                                               // the multiplied by 2 is for the opposite symmetrical face to reduce # of computations
 
         // TOP (posY):
         Vector3 pointVelPosY = rigidBod.GetPointVelocity(ypos_face_center);
-        Vector3 fluidDragVecPosY = -up * Vector3.Dot(up, pointVelPosY) * sa_y * viscosityDrag;
+        Vector3 fluidDragVecPosY = -up * LinearDragFloat(sa_y, Vector3.Dot(up, pointVelPosY));
         rigidBod.AddForceAtPosition(fluidDragVecPosY * 2, ypos_face_center);
 
         // RIGHT (posX):
         Vector3 pointVelPosX = rigidBod.GetPointVelocity(xpos_face_center);
-        Vector3 fluidDragVecPosX = -right * Vector3.Dot(right, pointVelPosX) * sa_x * viscosityDrag;
+        Vector3 fluidDragVecPosX = -right * LinearDragFloat(sa_x, Vector3.Dot(right, pointVelPosX));
         rigidBod.AddForceAtPosition(fluidDragVecPosX * 2, xpos_face_center);
 
     }
